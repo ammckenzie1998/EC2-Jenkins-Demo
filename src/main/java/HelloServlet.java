9fbc6324a95031536a45c0bf62fa9cf95b228a15
unknown <ammckenzie1998@gmail.com>
2021-01-05 15:58:14 -0500

    Added very basic HelloServlet

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/api/*")
+public class HelloServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+		throws ServletException, IOException{
+		
+		PrintWriter pw = resp.getWriter();
+		pw.write("<h1>This is my Jenkins EC2 demo!</h1>");
+		
+	}
+	
+	
+}
